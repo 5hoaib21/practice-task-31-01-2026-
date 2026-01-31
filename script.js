c07049b6f8ff4316a284b69c40d95fc276d9b142
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-01-31 09:31:25 +0600

    loopinnnngngngngngngn

--- script.js
+++ script.js
@@ -11,21 +11,99 @@
 //
 //
 
-function getGrade(score) {
-  if (score >= 90 && score <= 100) {
-    return "A";
-  } else if (score >= 80 && score <= 89) {
-    return "B";
-  } else if (score >= 70 && score <= 79) {
-    return "C";
-  } else if (score >= 60 && score <= 69) {
-    return "D";
-  } else if (score >= 33 && score <= 59) {
-    return "E";
-  } else if (score >= 0 && score <= 32) {
-    return "fail";
-  } else {
-    return "Invalid Marks `X` ";
-  }
-}
-console.log(getGrade(33));
+// function getGrade(score) {
+//   if (score >= 90 && score <= 100) {
+//     return "A";
+//   } else if (score >= 80 && score <= 89) {
+//     return "B";
+//   } else if (score >= 70 && score <= 79) {
+//     return "C";
+//   } else if (score >= 60 && score <= 69) {
+//     return "D";
+//   } else if (score >= 33 && score <= 59) {
+//     return "E";
+//   } else if (score >= 0 && score <= 32) {
+//     return "fail";
+//   } else {
+//     return "Invalid Marks `X` ";
+//   }
+// }
+// console.log(getGrade(33));
+///
+
+// let i = 1;
+// while (i < 32) {
+//   console.log("i");
+//   i++;
+// }
+
+// let i = 12;
+// do {
+//   console.log("i");
+//   i++;
+// } while (i < 2);
+
+// for (let i = 1; i < 201; i++) {
+//   if (i === 32) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// print even number from 1-20 using for loop
+
+// for (let i = 1; i < 21; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// print odd number from 1-15 using while loop
+
+// let i = 1;
+// while (i <= 15) {
+//   if (i % 2 === 1) console.log(i);
+//   i++;
+// }
+
+// let i = 1;
+// for (i = 1; i <= 50; i++) {
+//   if (i % 5 === 0) console.log(i);
+// }
+
+// let sum = 0;
+// for (let i = 1; i < 101; i++) {
+//   sum = sum + i;
+// }
+// console.log(sum);
+
+//
+//
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 3 === 0) {
+//     console.log(i);
+//   }
+// }
+
+//
+// let num = prompt("give a number");
+
+// for (let i = 1; i <= num; i++) {
+//   if (i % 2 === 0) {
+//     console.log(`${i} is even number`);
+//   } else {
+//     console.log(`${i} in odd number`);
+//   }
+// }
+
+//
+
+//
+
+// let i = 1;
+// for (i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
